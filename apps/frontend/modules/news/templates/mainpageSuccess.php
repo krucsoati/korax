e5af0a5fc665f22cab658826c7452889aed32c62
krucsoati <krucsoati@freemail.hu>
2012-07-24 22:48:10 +0200

    három sávos design

--- apps/frontend/modules/news/templates/mainpageSuccess.php
+++ apps/frontend/modules/news/templates/mainpageSuccess.php
@@ -6,7 +6,7 @@
 		<font color="#004D06" size="4">Villamosipari műgyanták</font>
 	    </b>
 	</p>
-	<img border="0" src="/img/villamosipari_01.jpg" >
+	<img border="0" src="/img/villamosipari_01_old.jpg" >
     </a>
 </div>
 
@@ -18,9 +18,31 @@
 		<font color="#004D06" size="4">Műgyantás padlók</font>
 	    </b>
 	</p>
-	<img border="0" src="/img/padlo_01.jpg" width="440" height="330">
+	<img border="0" src="/img/padlo_01_old.jpg" width="440" height="330">
     </a>
 </div>
+
+<table border="0" width="900" cellspacing="0" cellpadding="0" height="400">
+	<tr>
+		<td width="300" height="400%">
+                    <p align="left" style="margin-top: 0; margin-bottom: 0">&nbsp;&nbsp;&nbsp;&nbsp;
+		<b><font color="#004D06" size="4">Műgyantás padlók</font></b></p>
+		<p align="center" style="margin-top: 0; margin-bottom: 0">
+		<img border="0" src="/img/padlo_01.jpg" width="290" height="330"></p>
+		</td>
+		<td width="300" height="400">
+		<p align="center" style="margin-top: 0; margin-bottom: 0"><b>
+		<font size="4" color="#004D06">Lakosság termékek</font></b></p>
+		<p align="center" style="margin-top: 0; margin-bottom: 0">
+		<img border="0" src="/img/lakossagi_01.jpg" width="290" height="330"></td>
+		<td width="300" height="400">
+		<p align="right" style="margin-top: 0; margin-bottom: 0"><b>
+		<font color="#004D06" size="4">Villamosipari műgyanták&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </font></b></p>
+		<p align="center" style="margin-top: 0; margin-bottom: 0">
+		<img border="0" src="/img/villamosipari_01.jpg" width="290" height="330"></p>
+		</td>
+	</tr>
+</table>
 
 <?php foreach($news as $n): ?>
     <div class="news_box" >
